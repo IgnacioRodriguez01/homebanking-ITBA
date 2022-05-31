b329e03beb9d582277b213f2452c4b8284ffb4b7
dgamez03 <105295152+dgamez03@users.noreply.github.com>
2022-05-31 18:50:49 -0300

    Add files via upload

---
+++ List/main.js
@@ -0,0 +1,33 @@
+const dateNumber = document.getElementById('dateNumber');
+const dateText = document.getElementById('dateText');
+const dateMonth = document.getElementById('dateMonth');
+const dateYear = document.getElementById('dateYear');
+
+// Tasks Container
+const tasksContainer = document.getElementById('tasksContainer');
+
+const setDate = () => {
+    const date = new Date();
+    dateNumber.textContent = date.toLocaleString('es', { day: 'numeric' });
+    dateText.textContent = date.toLocaleString('es', { weekday: 'long' });
+    dateMonth.textContent = date.toLocaleString('es', { month: 'short' });
+    dateYear.textContent = date.toLocaleString('es', { year: 'numeric' });
+};
+
+const addNewTask = event => {
+    event.preventDefault();
+    const { value } = event.target.fecha && event.target.taskText;
+    if(!value) return;
+    const task = document.createElement('div');
+    task.classList.add('task', 'roundBorder');
+    task.addEventListener('click', changeTaskState)
+    task.textContent = value;
+    tasksContainer.prepend(task);
+    event.target.reset();
+};
+
+var changeTaskState = function (){
+        this.parentNode.removeChild(this);
+};
+
+setDate();
