dec9153bb7026447c1387fcd55d62e94acb00acb
Ignacio Rodriguez <ignacioandresrodriguez01@gmail.com>
2022-06-19 17:00:38 -0300

    Fixes en conversor

--- js/conversor.js
+++ js/conversor.js
@@ -5,8 +5,21 @@ const cantidad_two = document.getElementById("cantidad-dos")
 const cambioEl = document.getElementById("cambio")
 const taza = document.getElementById("taza")
 
+document.addEventListener('DOMContentLoaded', calculate)
 
-function calculate(){
+moneda_one.addEventListener("change", e => calculate(e.target));
+cantidad_one.addEventListener("input", e => calculate(e.target));
+moneda_two.addEventListener("change", e => calculate(e.target));
+cantidad_two.addEventListener("input", e => calculate(e.target));
+
+taza.addEventListener('click', (e) =>{
+    const temp = moneda_one.value;
+    moneda_one.value = moneda_two.value;
+    moneda_two.value = temp;
+    calculate(e.target);
+});
+
+function calculate(target){
 
     const moneda1 = moneda_one.value;
     const moneda2 = moneda_two.value;
@@ -14,21 +27,19 @@ function calculate(){
     fetch(`https://api.exchangerate-api.com/v4/latest/${moneda1}`)
     .then(res => res.json())
     .then(data=> {
-        const taza = data.rates[moneda2];
-        cambioEl.innerHTML= `1 ${moneda1} = ${taza} ${moneda2}`
-        cantidad_two.value = (cantidad_one.value * taza).toFixed(2);
-});
+        const conversion = data.rates[moneda2];
+        cambioEl.innerHTML= `1 ${moneda1} = ${conversion} ${moneda2}`
+        
+        if(target == taza) {
+            const valueStore = cantidad_two.value
+            cantidad_two.value = cantidad_one.value;
+            cantidad_one.value = valueStore;
+            return;
+        }
+        if(target == cantidad_two || target == moneda_two) {
+            cantidad_one.value = (cantidad_two.value / conversion).toFixed(3);
+            return;
+        }
+        cantidad_two.value = (cantidad_one.value * conversion).toFixed(3);
+    });
 }
-
-moneda_one.addEventListener("change", calculate);
-cantidad_one.addEventListener("input", calculate);
-moneda_two.addEventListener("change", calculate);
-cantidad_two.addEventListener("input", calculate);
-
-taza.addEventListener('click', () =>{
-    const temp = moneda_one.value;
-    moneda_one.value = moneda_two.value;
-    moneda_two.value = temp;
-    calculate();
-} );
-calculate();
